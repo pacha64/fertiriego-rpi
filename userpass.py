96da2e5ed55b04b19b20f5ce0abd609ca25c7992
Fernando Leboran <f.leboran@gmail.com>
2019-06-14 11:27:05 -0300

    updates

--- userpass.py
+++ userpass.py
@@ -1,4 +1,4 @@
-with open('userpass') as f:
+with open('/home/pi/fertiriego-rpi/userpass') as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content]
