1c8fe1a48a20c6dab8b7d8618e763d1764398675
Fernando Leboran <f.leboran@gmail.com>
2019-06-14 11:23:16 -0300

    updates

--- userpass.py
+++ userpass.py
@@ -1,5 +1,10 @@
+with open('userpass') as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+content = [x.strip() for x in content]
+
 def getUsername():
-    return "loretto"
+    return content[0]
 
 def getPassword():
-    return "quinta123"
+    return content[1]
