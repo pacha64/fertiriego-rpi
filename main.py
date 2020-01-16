c127f7cb48e20e8f72a69113e04022f14df7c38b
Fernando Leboran <f.leboran@gmail.com>
2020-01-16 11:28:17 -0300

    updates

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from userpass import getUsername, getPassword
 
 USE_RPI = True
 
-CURRENT_VERSION = 39
+CURRENT_VERSION = 40
 USERNAME = getUsername()
 PASSWORD = getPassword()
 URL_SERVER = 'http://emiliozelione2018.pythonanywhere.com/'
